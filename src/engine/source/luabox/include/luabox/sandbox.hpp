08ad288a9227327aa95709be8d5dc0544234f9c0
Javier Bejar Mendez <javier.bejar@wazuh.com>
2023-03-02 11:17:18 +0000

    Skeleton lua module added

---
+++ src/engine/source/luabox/include/luabox/sandbox.hpp
@@ -0,0 +1,13 @@
+#ifndef _LUABOX_SANDBOX_HPP
+#define _LUABOX_SANDBOX_HPP
+
+extern "C"
+{
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
+
+
+
+#endif // _LUABOX_SANDBOX_HPP
---
+++ src/engine/test/source/luabox/test.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <luabox/sandbox.hpp>
+
+TEST(LuaboxTest, test)
+{
+
+
+}
